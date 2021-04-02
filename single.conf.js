27307da58c8b323a9cfa93a2a213d4ae558ec51d
Rachna Agrawal <44583054+rachnaagrawal@users.noreply.github.com>
2021-04-02 22:54:30 +0530

    Update single.conf.js
    updated single.conf.js

--- single.conf.js
+++ single.conf.js
@@ -3,7 +3,7 @@ username= process.env.LT_USERNAME || "<your username>",
 accessKey=  process.env.LT_ACCESS_KEY || "<your accessKey>",
 
 exports.config = {
-  'specs': ['./specs/single.js'],
+  'specs': ['../specs/single.js'],
 
   seleniumAddress: 'https://'+ username +':'+ accessKey  +'@hub.lambdatest.com/wd/hub',
 
